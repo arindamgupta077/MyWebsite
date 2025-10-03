a4d6504147d037a3235ac11532bfe9cda00db0d8
Arindam Gupta <arindamgupta077@gmail.com>
2025-10-03 22:50:44 +0530

    Ready for hosting 2

--- next.config.js
+++ next.config.js
@@ -23,6 +23,14 @@ module.exports = {
       },
     ],
   },
-  // Disable strict mode that can cause issues with some third-party libraries
-  reactStrictMode: false,
+  // Enable React strict mode for better development experience
+  reactStrictMode: true,
+  // Webpack configuration to help with chunk loading issues
+  webpack: (config, { isServer }) => {
+    // Increase chunk timeout for development
+    if (!isServer) {
+      config.output.chunkLoadTimeout = 30000;
+    }
+    return config;
+  },
 }
